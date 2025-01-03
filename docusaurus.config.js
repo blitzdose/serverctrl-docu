49b194350b39b0ce9a36144a1adc548b0b065f11
Christian Zäske <blitzdose@gmail.com>
2025-01-03 18:41:23 +0100

    initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,157 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'ServerCtrl',
+  // tagline: 'Dinosaurs are cool',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://serverctrl.blitzdose.de',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'blitzdose', // Usually your GitHub org/user name.
+  projectName: 'serverctrl-docu', // Usually your repo name.
+  trailingSlash: false,
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          //editUrl:
+          //  'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        /*blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          //editUrl:
+          //  'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },*/
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      colorMode: {
+        defaultMode: 'dark',
+        respectPrefersColorScheme: true,
+        disableSwitch: false
+      },
+      // Replace with your project's social card
+      image: 'img/socialcard.jpg',
+      navbar: {
+        title: 'ServerCtrl',
+        logo: {
+          alt: 'ServerCtrl Logo',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'mainSidebar',
+            position: 'left',
+            label: 'Documentation',
+          },
+          //{to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/blitzdose/ServerCtrl',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Documentation',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Discord',
+                href: 'https://discord.gg/SewjCwVpaa',
+              },
+            ],
+          },
+          {
+            title: 'Download',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/blitzdose/ServerCtrl/releases/latest',
+              },
+              {
+                label: 'SpigotMC',
+                href: 'https://www.spigotmc.org/resources/serverctrl.72231',
+              },
+              {
+                label: 'Modrinth',
+                href: 'https://modrinth.com/plugin/serverctrl',
+              },
+              {
+                label: 'Google Play',
+                href: 'https://play.google.com/store/apps/details?id=de.blitzdose.minecraftserverremote',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} blitzdose. Built with Docusaurus ❤️`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,64 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: 'Control from everywhere',
+    Svg: require('@site/static/img/devices.svg').default,
+    description: (
+      <>
+       ServerCtrl lets you control you Spigot/Paper server from any device though
+       native apps or the universal web interface.
+      </>
+    ),
+  },
+  {
+    title: 'Beautiful UI',
+    Svg: require('@site/static/img/flower.svg').default,
+    description: (
+      <>
+        Say goodbye to ugly web panels and complicated functionality. ServerCtrl makes
+        it easy to control your server.
+      </>
+    ),
+  },
+  {
+    title: 'Open Source',
+    Svg: require('@site/static/img/code.svg').default,
+    description: (
+      <>
+        ServerCtrl is fully open source. Found flaws in the code? 
+        <a target='_blank' href='https://github.com/blitzdose/ServerCtrl/issues'>Tell me!</a>
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
